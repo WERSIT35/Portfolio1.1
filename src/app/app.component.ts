16878c03ee407a6731f597403d9d10e83ed67503
Oto <okupara123@gmail.com>
2024-10-24 19:59:24 +0400

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { LoadingSpinnerComponent } from "./components/loading-spinner/loading-spinner.component";
+import { LoadingService } from './services/loading.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet,
+     HeaderComponent,
+      LoadingSpinnerComponent,
+      CommonModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'portfolio1.1';
+export class AppComponent implements OnInit{
+  loading$ = this.loadingService.loading$;
+
+  constructor(private router: Router, private loadingService: LoadingService) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.loadingService.show();
+      } else if (event instanceof NavigationEnd) {
+        this.loadingService.hide();
+      }
+    });
+  }
 }
