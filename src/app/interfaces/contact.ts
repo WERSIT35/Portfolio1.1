bc0b8aaf0130408585cee7d10a07365f84bf2b4d
Oto <okupara123@gmail.com>
2024-10-24 20:53:50 +0400

    commit

---
+++ src/app/interfaces/contact.ts
@@ -0,0 +1,14 @@
+export interface Contact {
+    name:string,
+    surname:string,
+    image:string,
+    phone:string,
+    X:string,
+    instagram:string,
+    facebook:string,
+    email:string,
+    github:string,
+    linkedin:string,
+
+    location:string,
+}
